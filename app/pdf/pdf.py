524a77264c30fa56d66b2fd533e17007c0dba06c
abdoulfataoh <abdoulfataoh@gmail.com>
2023-05-26 21:24:01 +0000

    pdf module refactoring

--- app/pdf/pdf.py
+++ app/pdf/pdf.py
@@ -8,17 +8,15 @@ import fitz
 
 logger = logging.getLogger(__name__)
 
+
 class PdfMerger:
 
-    _pdf_folder: Path
     _pdf_files: List[Path]
 
-    def __init__(self, pdf_folder: Path) -> None:
-        logger.info(f'init pdfs files merging at {pdf_folder}')
-        self._pdf_folder = pdf_folder
-        find_pdf_query = pdf_folder.rglob('*.pdf')
-        self._pdf_files = list(find_pdf_query)
-        self._pdf_files.sort()
+    def __init__(self, pdfs_paths: List[Path], sort: bool = False) -> None:
+        self._pdf_files = pdfs_paths
+        if sort:
+            self._pdf_files.sort()
 
     def merge(self, output: Path) -> None:
         merger_document = fitz.open()
